f9af73be1275e023b78892862247203cb5e34067
Martinatjie <martinabrockmann@gmail.com>
2025-02-08 15:51:12 +0200

    added CRUD boilerplate for prompts

---
+++ NudgeBox.Api/NudgeBox.Api/Models/Prompt.cs
@@ -0,0 +1,11 @@
+﻿namespace NudgeBox.Api.Models
+{
+    public class Prompt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ NudgeBox.Api/NudgeBox.Api/Services/PromptService.cs
@@ -0,0 +1,36 @@
+﻿using NudgeBox.Api.Models;
+
+namespace NudgeBox.Api.Services
+{
+    public interface IPromptService
+    {
+        Prompt GetPrompt(int promptId);
+        Prompt CreatePrompt(Prompt prompt);
+        Prompt UpdatePrompt(Prompt prompt);
+        Prompt DeletePrompt(int promptId);
+    }
+
+    public class PromptService : IPromptService
+    {
+        public Prompt CreatePrompt(Prompt prompt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prompt DeletePrompt(int promptId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prompt GetPrompt(int promptId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prompt UpdatePrompt(Prompt prompt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
